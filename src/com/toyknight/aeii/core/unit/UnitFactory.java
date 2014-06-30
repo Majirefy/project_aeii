ed8f25fafd901a5c18b3226457d0461ef4b06d40
st000120 <st000120@WSH-ST000120.wkscn.wistron>
2014-06-30 16:19:23 +0800

    Unit包更新

---
+++ src/com/toyknight/aeii/core/unit/UnitFactory.java
@@ -0,0 +1,16 @@
+
+package com.toyknight.aeii.core.unit;
+
+/**
+ *
+ * @author st000120
+ */
+public class UnitFactory {
+    
+    private UnitFactory() {}
+    
+    public static Unit getUnit(String type) {
+        return null;
+    }
+    
+}
