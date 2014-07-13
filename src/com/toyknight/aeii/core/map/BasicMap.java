e1c81864461e960d70b94ad5093c1dd5f0ed6358
toyknight <toyknight@CMCC-EDU>
2014-07-13 21:35:59 +0800

    change MapData class name to EditorData

--- src/com/toyknight/aeii/core/map/BasicMap.java
+++ src/com/toyknight/aeii/core/map/BasicMap.java
@@ -1,10 +1,24 @@
 
 package com.toyknight.aeii.core.map;
 
+import com.toyknight.aeii.core.unit.Unit;
+import java.util.ArrayList;
+
 /**
  *
  * @author toyknight
  */
 public class BasicMap {
+	
+	private final ArrayList<Unit> unit_list = new ArrayList();
+	
+	public final Unit getUnit(int x, int y) {
+		for(Unit unit: unit_list) {
+			if(unit.getXPosition() == x && unit.getYPosition() == y) {
+				return unit;
+			}
+		}
+		return null;
+	}
 	
 }
