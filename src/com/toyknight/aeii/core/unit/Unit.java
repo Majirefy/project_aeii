96662d1a2294b168908e84cd813fecfbefbc94f1
st000120 <st000120@WSH-ST000120.wkscn.wistron>
2014-07-15 11:10:46 +0800

    Unit update

--- src/com/toyknight/aeii/core/unit/Unit.java
+++ src/com/toyknight/aeii/core/unit/Unit.java
@@ -10,39 +10,39 @@ import java.util.ArrayList;
 public class Unit {
    
     private int type;
-    private int grade;     //等级（初始等级为0，最高可以升级到3级）
+    private int level;     //等级（初始等级为0，最高可以升级到3级）
     private String unit_code;
     private int team;
     private int max_hp;
     private int current_hp;
     private int attack;
+    public static final int physical = 0;
+    public static final int magic = 1;
     private int attack_type;  //攻击类型整型（0：表示为物理攻击，1：表示魔法攻击）
     private int physical_defence;
-    private int magic_defence;
+    private int magical_defence;
     private int movement_point;
+    private int current_movement_point;
     private ArrayList<Integer> abilities;
-    private int state;
+   // private int state;
     private int hp_growth;
     private int attack_growth;
     private int physical_defence_growth;
-    private int magic_defence_growth;
+    private int magical_defence_growth;
     private int movement_growth;
     private int x_position;
     private int y_position;
-    private Unit unit;         //得到自身变量
-    private int max_spell_range;  //最大施法距离
-    private int min_spell_range;   //最小施法距离
+    private final Unit unit;         //得到自身变量
+    private int max_attack_range;  //最大攻击距离
+    private int min_attack_range;   //最小攻击距离
     
-    public Unit getUnit () {
-        return unit;
-    }
-    
-    public void setUnit(Unit unit) {
+    public Unit(Unit unit) {
         this.unit = unit;
     }
      
     public int getType() {
         return type;
+        
     }
     
     public void setType(int type) {
@@ -50,15 +50,15 @@ public class Unit {
       this.type = type;
     }
     
-    public int getGrade() {
-        if(grade == 3) {
+    public int getLevel() {
+        if(level == 3) {
             this.getAbilities();
         }
-        return grade;
+        return level;
     }
     
-    public void setGrade(int grade) {
-        this.grade = grade;
+    public void setLevel(int level) {
+        this.level = level;
     }
       public String getUnitCode() {
         return unit_code;
@@ -115,12 +115,12 @@ public class Unit {
         this.physical_defence = physical_defence;
     }
     
-    public int getMagicDefence() {
-        return magic_defence;
+    public int getMagicalDefence() {
+        return magical_defence;
     }
     
-    public void setMagicDefence(int magic_defence) {
-        this.magic_defence = magic_defence;
+    public void setMagicalDefence(int magical_defence) {
+        this.magical_defence = magical_defence;
     }
     
     public int getMovementGrowth() {
@@ -138,6 +138,13 @@ public class Unit {
         this.movement_point = movement_point;
     }
     
+    public int getCurrentMovementPoint() {
+        return current_movement_point;
+    }
+    
+    public void setCurrentMovementPoint(int current_movement_point) {
+        this.current_movement_point = current_movement_point;
+    }
     public ArrayList<Integer> getAbilities() {
         return abilities;
     }
@@ -146,14 +153,14 @@ public class Unit {
         this.abilities = abilities;
     }
     
-      public int getState() {
+ /*     public int getState() {
         return state;
     }
     
     public void setState(int state) {
         this.state = state;
     }
-    
+  */ 
       public int getHpGrowth() {
         return hp_growth;
     }
@@ -174,12 +181,12 @@ public class Unit {
         this.physical_defence_growth = physical_defence_growth;
     }
     
-    public int getMagicDefenceGrowth() {
-        return magic_defence_growth;
+    public int getMagicalDefenceGrowth() {
+        return magical_defence_growth;
     }
     
-    public void setMagicDefenceGrowth(int magic_defence_growth) {
-        this.magic_defence_growth = magic_defence_growth;
+    public void setMagicalDefenceGrowth(int magical_defence_growth) {
+        this.magical_defence_growth = magical_defence_growth;
     }
     
     public int getXPosition() {
@@ -198,19 +205,19 @@ public class Unit {
         this.y_position = y_position;
     } 
 
-    public int getMaxSpellRange() {
-        return max_spell_range;
+    public int getMaxAttackRange() {
+        return max_attack_range;
     }
     
-    public void setMaxSpellRange(int max_spell_range) {
-        this.max_spell_range = max_spell_range;
+    public void setMaxAttackRange(int max_attack_range) {
+        this.max_attack_range = max_attack_range;
     }
     
-    public int getMinSpellRange() {
-        return min_spell_range;
+    public int getMinAttackRange() {
+        return min_attack_range;
     }
     
-    public void setMinSpellRange(int min_spell_range) {
-        this.min_spell_range = min_spell_range;
+    public void setMinAttackRange(int min_attack_range) {
+        this.min_attack_range = min_attack_range;
     }
 }
