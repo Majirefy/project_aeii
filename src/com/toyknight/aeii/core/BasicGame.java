2b8e4e6bc1f2cd665221d97a325890fc4f595816
st000121 <st000121@WSH-ST000121.wkscn.wistron>
2014-07-21 17:49:05 +0800

    core update

--- src/com/toyknight/aeii/core/BasicGame.java
+++ src/com/toyknight/aeii/core/BasicGame.java
@@ -1,13 +1,19 @@
 
 package com.toyknight.aeii.core;
 
+import com.toyknight.aeii.core.map.Map;
+
 /**
  *
  * @author toyknight
  */
 public class BasicGame {
 	
-	public BasicGame() {
+	public BasicGame(Map map) {
+		
+	}
+	
+	public void endTurn() {
 		
 	}
 	
