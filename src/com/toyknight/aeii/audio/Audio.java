3461bf92deb77083944c5166363c5c05960bb969
s1407003 <s1407003@WSH-S1407003.wkscn.wistron>
2014-11-19 10:38:10 +0800

    audio_test Update

--- src/com/toyknight/aeii/audio/Audio.java
+++ src/com/toyknight/aeii/audio/Audio.java
@@ -27,131 +27,141 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 public class Audio extends Thread {
 
-	/**
-	 * 1.定义音频文件的变量，变量需要： 一个用于存储音频文件对象名字的String对象 filename； 2.构造函数，初始化filename
-	 * 3.线程运行函数重写
-	 *	 
+    /**
+     * 1.定义音频文件的变量，变量需要： 一个用于存储音频文件对象名字的String对象 filename； 2.构造函数，初始化filename
+     * 3.线程运行函数重写
+     *     
 */
 //1.定义音频文件的变量，变量需要：一个用于存储音频文件对象名字的String对象 filename
-	private final String filename;
-	private final boolean loop;
-	private boolean isRunning = true;
-	private AudioListener al = null;
-	private AudioListenerVolumn alv = null;
-	private SourceDataLine auline = null;
-	private AudioFormat format = null;
+    private final String filename;
+    private final boolean loop;
+    private boolean isRunning = true;
+    private AudioListener al = null;
+    private AudioListenerVolumn alv = null;
+    private SourceDataLine auline = null;
+    private AudioFormat format = null;
+    private boolean isWave = true;
 
 //2.构造函数，初始化filename
-	public Audio(String filename, boolean loop) {
-		this.filename = filename;
-		this.loop = loop;
+    public Audio(String filename, boolean loop) {
+        this.filename = filename;
+        this.loop = loop;
 
-	}
+    }
 
-	public void Stop() {
-		isRunning = false;
-	}
+    public void Stop() {
+        isRunning = false;
+    }
 
-	public void addAudioListener(AudioListener al) {
-		this.al = al;
-	}
+    public void addAudioListener(AudioListener al) {
+        this.al = al;
+    }
 
-	public void finish() {
-		if (al != null) {
-			al.nextMusic();
-		}
-	}
+    public void finish() {
+        if (al != null) {
+            al.nextMusic();
+        }
+    }
 
-	public void initAudioVolumn(AudioListenerVolumn alv) {
-		this.alv = alv;
-	}
+    public void initAudioVolumn(AudioListenerVolumn alv) {
+        this.alv = alv;
+    }
 
-	public void initVolumn() {
-		alv.initMusicVolumn();
-	}
+    public void initVolumn() {
+        alv.initMusicVolumn();
+    }
 
 //3.线程运行函数重写  
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 //1.定义一个文件对象引用，指向名为filename那个文件
-		File sourceFile = new File(filename);
-		// System.out.println(sourceFile.getAbsolutePath());
+        File sourceFile = new File(filename);
+        // System.out.println(sourceFile.getAbsolutePath());
 //2定义一个AudioInputStream用于接收输入的音频数据
-		AudioInputStream audioInputStream = null;
+        AudioInputStream audioInputStream = null;
+        AudioInputStream othersAudioInputStream = null;
 //3使用AudioSystem来获取音频的音频输入流
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(sourceFile);
-		} catch (UnsupportedAudioFileException | IOException e) {
-			System.err.println(e.getClass().toString() + ": " + e.getMessage());
-		}
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(sourceFile);
+        } catch (UnsupportedAudioFileException e) {
+        } catch (IOException e) {
+        }
 //4,用AudioFormat来获取AudioInputStream的格式
-		format = audioInputStream.getFormat();
+        format = audioInputStream.getFormat();
 
-		if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
-			format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, format.getSampleRate(), 16, format.getChannels(), format.getChannels() * 2, format.getSampleRate(), false);
-			audioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream);
-		}
+        if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
+            format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, format.getSampleRate(), 16, format.getChannels(), format.getChannels() * 2, format.getSampleRate(),false);
+            othersAudioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream);
+            isWave = false;
+        }else {
+            othersAudioInputStream = audioInputStream;
+        }
 
 //        System.out.println(format.toString());
 //        System.out.println(format.getEncoding() + "\n" + format.getSampleRate() + "\n" + format.getSampleSizeInBits() + "\n" + format.getChannels() + "\n" + format.getFrameSize() + "\n" + format.getFrameRate() + "\n");
 //5.源数据行SoureDataLine是可以写入数据的数据行
 //获取受数据行支持的音频格式DataLine.info
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format, AudioSystem.NOT_SPECIFIED);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format, AudioSystem.NOT_SPECIFIED);
 
-		//获得与指定info类型相匹配的行
-		try {
-			auline = (SourceDataLine) AudioSystem.getLine(info);
+        //获得与指定info类型相匹配的行
+        try {
+            auline = (SourceDataLine) AudioSystem.getLine(info);
 ////打开具有指定格式的行，这样可使行获得所有所需系统资源并变得可操作
-			auline.open();
+            auline.open();
 
-		} catch (LineUnavailableException e) {
-		}
+        } catch (LineUnavailableException e) {
+        }
 //允许某一个数据行执行数据i/o
-		auline.start();
+        auline.start();
 
-		initVolumn();
+        initVolumn();
 
 //写出数据
-		int nBytesRead = 0;
-		//System.out.println(audioInputStream.markSupported());
-		byte[] abData = new byte[48];
+        int nBytesRead = 0;
+        //System.out.println(audioInputStream.markSupported());
+        byte[] abData = new byte[48];
 //从音频流读取指定的最大数量的数据字节，并将其放入给定的字节数组中。
-		try {
-			while (nBytesRead != -1 && isRunning) {
-				nBytesRead = audioInputStream.read(abData, 0, abData.length);
+        try {
+            while (nBytesRead != -1 && isRunning) {
+                nBytesRead = othersAudioInputStream.read(abData, 0, abData.length);
 //通过此源数据行将数据写入混频器
-				if (nBytesRead >= 0) {
-					auline.write(abData, 0, nBytesRead);
-					// System.out.println(nBytesRead);
-				} else if (loop) {
-					audioInputStream = AudioSystem.getAudioInputStream(sourceFile);
-					nBytesRead = audioInputStream.read(abData, 0, abData.length);
-				}
-			}
-		} catch (IOException ex) {
-		} catch (UnsupportedAudioFileException ex) {
-			Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
-		} finally {
-			auline.drain();
-			auline.close();
-		}
-		if (isRunning) {
-			finish();
-		}
-	}
+                if (nBytesRead >= 0) {
+                    auline.write(abData, 0, nBytesRead);
+                    // System.out.println(nBytesRead);
+                } else if (loop) {
+                    audioInputStream = AudioSystem.getAudioInputStream(sourceFile);
+                    if(isWave) {
+                        othersAudioInputStream = audioInputStream;
+                    }else {
+                        othersAudioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream);
+                    }
+                    nBytesRead = othersAudioInputStream.read(abData, 0, abData.length);
+                }
+            }
+        } catch (IOException ex) {
+        } catch (UnsupportedAudioFileException ex) {
+            Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            auline.drain();
+            auline.close();
+        }
+        if (isRunning) {
+            finish();
+        }
+    }
 
-	public void changeVolumn(int volumn) {
-		float new_volumn = 30 + volumn * 70 / 100.0f;
-		FloatControl volctrl = (FloatControl) auline.getControl(FloatControl.Type.MASTER_GAIN);
-		float min = volctrl.getMinimum();
-		//System.out.println(min);
-		float max = volctrl.getMaximum();
-		//System.out.println(max);
-		float width = max - min;
-		float f_volumn = min + (new_volumn * width / 100.0f);
+    public void changeVolumn(int volumn) {
+        float new_volumn = 30 + volumn * 70 / 100.0f;
+        FloatControl volctrl = (FloatControl) auline.getControl(FloatControl.Type.MASTER_GAIN);
+        float min = volctrl.getMinimum();
+        //System.out.println(min);
+        float max = volctrl.getMaximum();
+        //System.out.println(max);
+        float width = max - min;
+        float f_volumn = min + (new_volumn * width / 100.0f);
 //        System.out.println(volumn);
 //        System.out.println(f_volumn);
-		volctrl.setValue(f_volumn);
+        volctrl.setValue(f_volumn);
 
-	}
+    }
 }
